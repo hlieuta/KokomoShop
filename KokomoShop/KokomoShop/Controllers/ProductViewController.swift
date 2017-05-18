82f05c69c0a6008db6696996a25580f0218efca1
Huy Ta <hlieuta@gmail.com>
2017-05-18 15:24:46 +1000

    Added pager view

---
+++ KokomoShop/KokomoShop/Controllers/ProductViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ProductViewController.swift
+//  KokomoShop
+//
+//  Created by Huy Ta on 5/18/17.
+//  Copyright © 2017 'Kokomo'. All rights reserved.
+//
+
+import UIKit
+import XLPagerTabStrip
+
+class ProductViewController: UIViewController, IndicatorInfoProvider {
+    
+    var itemInfo: IndicatorInfo = "View"
+    
+    init(itemInfo: IndicatorInfo) {
+        self.itemInfo = itemInfo
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "XLPagerTabStrip"
+        
+        view.addSubview(label)
+        view.backgroundColor = .white
+        
+        view.addConstraint(NSLayoutConstraint(item: label, attribute: .centerX, relatedBy: .equal, toItem: view, attribute: .centerX, multiplier: 1, constant: 0))
+        view.addConstraint(NSLayoutConstraint(item: label, attribute: .centerY, relatedBy: .equal, toItem: view, attribute: .centerY, multiplier: 1, constant: -50))
+    }
+    
+    // MARK: - IndicatorInfoProvider
+    
+    func indicatorInfo(for pagerTabStripController: PagerTabStripViewController) -> IndicatorInfo {
+        return itemInfo
+    }
+}
---
+++ KokomoShop/KokomoShop/Controllers/ShopButtonBarPagerTabStripViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ShopButtonBarPagerTabStripViewController.swift
+//  KokomoShop
+//
+//  Created by Huy Ta on 5/18/17.
+//  Copyright © 2017 'Kokomo'. All rights reserved.
+//
+
+import UIKit
+import XLPagerTabStrip
+
+class ShopButtonBarPagerTabStripViewController: ButtonBarPagerTabStripViewController {
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        buttonBarView.selectedBar.backgroundColor = .orange
+        buttonBarView.backgroundColor = UIColor(red: 7/255, green: 185/255, blue: 155/255, alpha: 1)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewControllers(for pagerTabStripController: PagerTabStripViewController) -> [UIViewController] {
+         return [ProductViewController(itemInfo: "View"), ProductViewController(itemInfo: "View 2"), ProductViewController(itemInfo: "View 2")]
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
