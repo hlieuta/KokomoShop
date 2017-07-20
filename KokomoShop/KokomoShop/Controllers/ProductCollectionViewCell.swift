51aed79608d9ce6a5400915194c1900137b39a9f
Huy Ta <hlieuta@gmail.com>
2017-07-20 13:30:21 +1000

    added image view

---
+++ KokomoShop/KokomoShop/Controllers/ProductCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ProductCollectionViewCell.swift
+//  KokomoShop
+//
+//  Created by Huy Ta on 7/20/17.
+//  Copyright © 2017 'Kokomo'. All rights reserved.
+//
+
+import UIKit
+
+class ProductCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var productName: UILabel!
+    @IBOutlet weak var productDescription: UILabel!
+    @IBOutlet weak var price: UILabel!
+}
--- KokomoShop/KokomoShop/Controllers/ProductViewController.swift
+++ KokomoShop/KokomoShop/Controllers/ProductViewController.swift
@@ -11,12 +11,15 @@ import XLPagerTabStrip
 import OperaSwift
 import RxSwift
 import RxCocoa
-
+import Nuke
 
 class ProductViewController: UIViewController, IndicatorInfoProvider {
     
     
     @IBOutlet weak var collectionView: UICollectionView!
+    
+    let refreshControl = UIRefreshControl()
+    var disposeBag = DisposeBag()
 
     var itemInfo: IndicatorInfo = "View"
     var categoryId: String = ""
@@ -31,19 +34,62 @@ class ProductViewController: UIViewController, IndicatorInfoProvider {
         super.init(coder: aDecoder)
     }
     
-    let disposeBag = DisposeBag()
 
     
     lazy var viewModel: PaginationViewModel<PaginationRequest<Product>> = { [unowned self] in
         return PaginationViewModel(paginationRequest: PaginationRequest(route: Route.Product.findProductsByCategory(categoryId: self.categoryId) ,query: "20", collectionKeyPath: "CatalogEntryView"))
     }()
     
+    fileprivate lazy var emptyStateLabel: UILabel = {
+        let emptyStateLabel = UILabel()
+        emptyStateLabel.text = "No products found"
+        emptyStateLabel.textAlignment = .center
+        return emptyStateLabel
+    }()
+    
  
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        collectionView.delegate = self
-        collectionView.dataSource = self
+        collectionView.backgroundView = emptyStateLabel
+        collectionView.addSubview(self.refreshControl)
+        let refreshControl = self.refreshControl
+        rx.sentMessage(#selector(ProductViewController.viewWillAppear(_:)))
+            .map { _ in false }
+            .bind(to: viewModel.refreshTrigger)
+            .addDisposableTo(disposeBag)
+        
+        collectionView.rx.reachedBottom
+            .bind(to: viewModel.loadNextPageTrigger)
+            .addDisposableTo(disposeBag)
+        
+        Driver.combineLatest(viewModel.elements.asDriver(), viewModel.firstPageLoading) { elements, loading in return loading ? [] : elements }
+            .asDriver()
+            .drive(collectionView.rx.items(cellIdentifier: "cell")) { _, product, cell in
+                let productCell = cell as! ProductCollectionViewCell
+                productCell.productName.text = product.name
+                productCell.productDescription.text = product.shortDescription
+                productCell.price.text = "$100"
+                productCell.imageView.image = nil
+                Nuke.loadImage(with: URL(string: Constants.Network.baseUrl.absoluteString + product.fullImage!)!, into: productCell.imageView)
+                
+            }
+            .addDisposableTo(disposeBag)
+        
+        refreshControl.rx.valueChanged
+            .filter { refreshControl.isRefreshing }
+            .map { true }
+            .bind(to: viewModel.refreshTrigger)
+            .addDisposableTo(disposeBag)
+        
+        viewModel.loading
+            .filter { !$0 && refreshControl.isRefreshing }
+            .drive(onNext: { _ in refreshControl.endRefreshing() })
+            .addDisposableTo(disposeBag)
+        
+        viewModel.emptyState
+            .drive(onNext: { [weak self] emptyState in self?.emptyStateLabel.isHidden = !emptyState })
+            .addDisposableTo(disposeBag)
         
     }
     // MARK: - IndicatorInfoProvider
